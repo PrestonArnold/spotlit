e859e42b6e7f1608a3e2fe7ed8b466e8872c37cd
Preston Arnold <prestonarnold19@gmail.com>
2026-03-28 18:18:43 +0000

    Fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,4 +4,4 @@ import interviewRouter from './routes/interview.js';
 
 app.use('/interview', interviewRouter)
 
-app.listen(69420, () => console.log('running on port 69420'))
+app.listen(3002, () => console.log('running on port 3002'))
