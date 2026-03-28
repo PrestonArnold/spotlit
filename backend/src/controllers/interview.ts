79a6a7a3f962e30e165fadcc7721f2ec22bc6933
Preston Arnold <prestonarnold19@gmail.com>
2026-03-28 17:48:08 +0000

    submitAnswer

--- backend/src/controllers/interview.ts
+++ backend/src/controllers/interview.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
+import { writeFileSync } from "fs";
+import { join } from "path";
 
 export const submitAnswer = (req: Request, res: Response) => {
     const { questionId, audioBase64 } = req.body;
-    // get buffer from base64
-    // save to user-recordings
-    // return response
+
+    const audioBuffer = Buffer.from(audioBase64, "base64");
+    const filePath = join(__dirname, "../user-recordings", `${questionId}.mp3`);
+    
+    writeFileSync(filePath, audioBuffer);
+
+    res.json({ status: "ok", file: filePath });
 }
 
 export const generateInterview = async (req: Request, res: Response) => {
