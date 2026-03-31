84bdc0f07e54cd2e8c9192d19650d6bcbbf328ca
Preston Arnold <66842041+PrestonArnold@users.noreply.github.com>
2026-03-31 13:13:28 +0100

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,5 +10,6 @@ export const app: Express = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
+// TODO
 app.use('/questions', express.static("/var/www/apis/src/questions/"))
 app.use('/interview', express.static("/var/www/apis/src/interviews/"))
